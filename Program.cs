a178d53ef979b5b0ef88efadd90594589fa62bdc
Artem023 <115217944+Artem023@users.noreply.github.com>
2023-01-03 15:54:41 +0300

    Creat method which would print array

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@ arr[1] = "Hi!";
 arr [2] = "HEllo!";
 arr [3] = "How Are YOU";
 arr [4] = "!";
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+}
