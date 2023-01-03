c528d77416fdd02a2d70e0ba6fb4bc458425c610
Artem023 <115217944+Artem023@users.noreply.github.com>
2023-01-03 15:56:46 +0300

    Creat method which would count elements less or equel 3

--- Program.cs
+++ Program.cs
@@ -15,3 +15,16 @@ void PrintArray(string[] array)
         else Console.Write($"{array[i]}]");
     }
 }
+
+int FindSizeOfArr(string [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
